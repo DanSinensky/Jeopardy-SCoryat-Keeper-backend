3837e0895c038854237be7218f1e080c38cd41e6
Dan Sinensky <dan.i.sinensky@gmail.com>
2024-05-07 15:16:51 -0400

    initial file structure, some files

---
+++ server.js
@@ -0,0 +1,8 @@
+//imports
+import db from "./db/connection.js";
+import express from "express";
+import routes from "./routes/index.js";
+import cors from "cors";
+import logger from "morgan";
+import chalk from "chalk";
+
