121ff51396211889f9100913d75d6b9ac3d72116
Dan Sinensky <dan.i.sinensky@gmail.com>
2024-06-06 21:15:26 -0400

    begin restructuring game model and seed

--- models/Game.js
+++ models/Game.js
@@ -2,10 +2,14 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const GameSchema = new Schema({
-  name: { type: String, required: true },
+  title: { type: String, required: true },
   scrapedID: {type: Number, required: true},
   date: { type: Date, required: true },
-  scores: [{ type: Schema.Types.ObjectId, ref: 'Score' }]
+  scores: [{ type: Schema.Types.ObjectId, ref: 'Score' }],
+  game_comments: { type: String },
+  categories: [{ type: String }],
+  category_comments: [{ type: String }],
+  jeopardy_round: 
 }, { timestamps: true });
 
 export default mongoose.model("Game", GameSchema);
