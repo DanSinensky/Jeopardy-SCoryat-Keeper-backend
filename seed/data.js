7e6a80282651a312520f6797c78279f5748f1f2b
Dan Sinensky <dan.i.sinensky@gmail.com>
2024-06-12 17:58:27 -0400

    adds score schema to seed, factors pagination and filter into writing api to json

--- seed/data.js
+++ seed/data.js
@@ -1,39 +1,49 @@
 import db from "../db/connection.js";
 import GameSchema from "../models/Game.js";
+import ScoreSchema from "../models/Score.js";
 import gameData from "./gameData.json" assert {type: "json"};
-
-let newGameData = gameData.map((game) => {
-  const gameData = {
-    game_title: game.game_title,
-    game_id: game.game_id,
-    game_date: game.game_date,
-    scores: game.scores,
-    game_comments: game.game_comments,
-    categories: game.categories,
-    category_comments: game.category_comments,
-    jeopardy_round: {
-      clues: game.jeopardy_round.clues,
-      responses: game.jeopardy_round.responses,
-      cells: game.jeopardy_round.cells
-    },
-    double_jeopardy_round: {
-      clues: game.double_jeopardy_round.clues,
-      responses: game.double_jeopardy_round.responses,
-      cells: game.double_jeopardy_round.cells
-    },
-    final_jeopardy: {
-      clue: game.final_jeopardy.clue,
-      response: game.final_jeopardy.response
-    }
-  };
-
-  return gameData;
-});
 
 const seedData = async () => {
   await db.dropDatabase();
+
+  const scorePromises = gameData.map(game => {
+    const scoreDocs = game.scores.map(score => ({ dollars: score.dollars }));
+    return ScoreSchema.insertMany(scoreDocs);
+  });
+
+  const allScores = await Promise.all(scorePromises);
+
+  const newGameData = gameData.map((game, index) => {
+    const gameScores = allScores[index].map(scoreDoc => scoreDoc._id);
+
+    return {
+      game_title: game.game_title,
+      game_id: game.game_id,
+      game_date: game.game_date,
+      scores: gameScores,
+      game_comments: game.game_comments,
+      categories: game.categories,
+      category_comments: game.category_comments,
+      jeopardy_round: {
+        clues: game.jeopardy_round.clues,
+        responses: game.jeopardy_round.responses,
+        cells: game.jeopardy_round.cells
+      },
+      double_jeopardy_round: {
+        clues: game.double_jeopardy_round.clues,
+        responses: game.double_jeopardy_round.responses,
+        cells: game.double_jeopardy_round.cells
+      },
+      final_jeopardy: {
+        clue: game.final_jeopardy.clue,
+        response: game.final_jeopardy.response
+      }
+    };
+  });
+
   await GameSchema.create(newGameData);
-  console.log("Games seeded to  Database");
+  console.log("Games seeded to Database");
+
   await db.close();
 };
 
--- seed/writeApiDataToJson.js
+++ seed/writeApiDataToJson.js
@@ -2,34 +2,56 @@ import axios from "axios";
 import fs from "fs";
 
 const apiGamesUrl = "https://jeopardy-scoryat-webscraper-464ba2509006.herokuapp.com/api/games";
+const pageSize = 100; 
 
-async function fetchGamesDataFromApi() {
+async function fetchGamesDataFromApi(page) {
   try {
-    const response = await axios.get(apiGamesUrl);
+    const response = await axios.get(apiGamesUrl, {
+      params: {
+        page: page,
+        size: pageSize
+      }
+    });
     return response.data;
   } catch (error) {
-    throw new Error("Failed to fetch data from API", error.message);
+    throw new Error(`Failed to fetch data from API on page ${page}: ${error.message}`);
   }
+}
+
+function filterGamesData(data) {
+  return data.filter(game => game.hasOwnProperty('game_date') && !game.hasOwnProperty('error'));
 }
 
 function writeGamesDataToJsonFile(data) {
   const gamesJsonData = JSON.stringify(data, null, 2);
 
-  fs.writeFile("a.json", gamesJsonData, (err) => {
+  fs.writeFile("gamesData.json", gamesJsonData, (err) => {
     if (err) {
-      throw new Error("Failed to write data to JSON file:", err.message);
+      throw new Error(`Failed to write data to JSON file: ${err.message}`);
     }
-    console.log("Data written to charData.json successfully");
+    console.log("Data written to gamesData.json successfully");
   });
 }
 
 async function main() {
+  let allGamesData = [];
+  let page = 1;
+  let totalPages;
+
   try {
-    const data = await fetchGamesDataFromApi();
-    writeGamesDataToJsonFile(data)
+    do {
+      const response = await fetchGamesDataFromApi(page);
+      totalPages = response.total_pages;
+      console.log(`Fetching page ${page} of ${totalPages}`);
+      const filteredGames = filterGamesData(response.games);
+      allGamesData = allGamesData.concat(filteredGames);
+      page++;
+    } while (page <= totalPages);
+
+    writeGamesDataToJsonFile(allGamesData);
   } catch (error) {
-    console.log(error)
+    console.error(error.message);
   }
 }
 
-main()
+main();
