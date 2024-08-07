4e4903cba0ec868e622cc303b0c770f21af4cbda
Dan Sinensky <dan.i.sinensky@gmail.com>
2024-08-07 19:59:34 -0400

    update Score

--- models/Score.js
+++ models/Score.js
@@ -4,7 +4,7 @@ const { Schema } = mongoose;
 const ScoreSchema = new Schema({
   dollars: { type: Number, required: true },
   userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  gameId: { type: Schema.Types.ObjectId, ref: 'Game', required: true }
+  game_id: { type: Number, required: true }
 }, { timestamps: true });
 
 export default mongoose.model("Score", ScoreSchema);
